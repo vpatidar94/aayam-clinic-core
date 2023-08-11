07d538008da23f8360a9c7640e447be6f5f33439
vinay patidar <vpatidar94@gmail.com>
2023-08-11 07:33:39 +0530

    ACL Vo

--- src/vo/auth/acl.vo.ts
+++ src/vo/auth/acl.vo.ts
@@ -3,5 +3,8 @@ export interface AclVo {
     subRole: string | null | undefined; // GENERAL_STAFF, DOCTOR, LAB_STAFF, CHEMIST
     orgId: string | null | undefined; // Org Id or School Id
     brId: string | null | undefined; // branch - brId=master for master branch level access
+    departmentId: string | null | undefined;
+    userTypeId: string | null | undefined;
+    designation: string | null | undefined; 
     active: boolean;
 }
